4b6ef5c99aeb70539d2faf3560c5aede333657c2
Say19705 <54776073+Say19705@users.noreply.github.com>
2020-03-20 08:57:55 -0600

    Update Prueba.java

--- src/Prueba.java
+++ src/Prueba.java
@@ -6,8 +6,28 @@ import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Prueba{
+	public static void main(String[] args) {
+        InputStreamReader ireader = new InputStreamReader(System.in);
+        BufferedReader breader = new BufferedReader(ireader);
+        LObj gEnv = Evaluator.globalEnv();
+        try {
+            String line;
+            System.out.print("> ");
+            while ((line = breader.readLine()) != null) {
+                System.out.print(Evaluator.eval(Reader.read(line).obj, gEnv));
+            System.out.print("\n> ");
+            }
+        } catch (IOException e) {}
+    }
+	
 	
 
 }

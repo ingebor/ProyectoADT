09d1a7103d2708ece336629112813bb848609a9c
Say19705 <54776073+Say19705@users.noreply.github.com>
2020-05-15 19:57:33 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -68,7 +68,82 @@ public class Main {
 					//System.out.println("entro2");
 					System.out.println(c.funcion(canti));
     			}
-    			else if(option == 3) {
+				else if (option == 3) {
+    				Controller c = new Controller();
+    				int predicado = 0;
+    				String codigo = teclado2.nextLine();
+    				int num = codigo.length();
+    				String[] palabra = codigo.split(" ");
+    				int pal = palabra.length;
+    				String completo = "";
+    				completo = c.separar(num, palabra, completo);
+    				
+    				for(int i = 0; i<pal; i++) {
+    					if(palabra[i].equals("(") || palabra[i].equals(")")) {
+    						
+    					}
+    					else {
+    						completo = completo + palabra[i];
+    					}
+    				}
+    				int cont = completo.length();
+    				String final1 = "";
+    				
+    				for(int i = 0; i < cont; i++) {
+    					if(completo.substring(i,i+1).equals("<")) {
+    						predicado = 1;
+    					}
+    					else if(completo.substring(i,i+1).equals(">")) {
+    						predicado = 2;
+    					}
+    					else if (completo.substring(i,i+1).equals("=")) {
+    						predicado = 3;
+    					}
+    					else if (completo.substring(i,i+1).equals("ATOM")) {
+    						predicado = 4;
+    					}
+    					else {
+    						final1 = final1 + completo.substring(i,i+1);
+    					}
+    				}
+    				int numero1 = Integer.parseInt(final1.substring(0, 1));
+    				int numero2 = Integer.parseInt(final1.substring(1, 2));
+    				
+    				if(predicado==1) {
+    					if(numero1<numero2) {
+    						System.out.println("T");
+    						}
+    					else {
+    						System.out.println("NIL");
+    						}
+    					}
+    				else if(predicado==2) {
+    					if(numero1>numero2) {
+    						System.out.println("T");
+    						}
+    					else {
+    						System.out.println("NIL");
+    						}
+    					}
+    				else if(predicado==3) {
+    					if(numero1 == numero2) {
+    						System.out.println("T");
+    						}
+    					else {
+    						System.out.println("NIL");
+    						}
+    					}
+    				}
+    			else if (option == 4) {
+    				String codigo = teclado2.nextLine();
+    				int num = codigo.length();
+    				String[] palabra = codigo.split(" ");
+    				
+    			}
+    			else if (option == 5) {
+    				
+    			}
+    			else if(option == 6) {
     				flag=false;
     			}
     		}

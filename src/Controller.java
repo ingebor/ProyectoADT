bffb9a5c048e1623773cba1364e6a2e6f5d7cada
Say19705 <54776073+Say19705@users.noreply.github.com>
2020-05-15 21:27:57 -0600

    Update Controller.java

--- src/Controller.java
+++ src/Controller.java
@@ -287,6 +287,15 @@ public class Controller {
 				}
 			}
 		}
+		
+	public boolean isNumeric(String str) { 
+		  try {  
+		    Double.parseDouble(str);  
+		    return true;
+		  } catch(NumberFormatException e){  
+		    return false;  
+		  }  
+		}
 	
 
 }
